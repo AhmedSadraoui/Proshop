2da2ff36a4c942d7f3c7a92a82eeec9ed60db8fe
Ahmed Sadraoui <ahmedsadraouiu777@gmail.com>
2023-05-27 08:53:33 +0100

    Updated app with new changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,7 +14,6 @@ export const HomeScreen = () => {
         {latestProducts.map((product)=>(
 
                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}><Product product={product}/></Col>
-                       
         ))}
     <h1>Gaming Products</h1>
     <Row>
